736a618b09349004454ed6718979378b4d943aef
Ayman <ayman.farhat1@gmail.com>
2017-04-17 09:44:35 +0300

    Switch to a copy of the simple theme to build on top

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ DEFAULT_PAGINATION = 10
 ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
 ARTICLE_URL = '{category}/{slug}'
 
-THEME = './themes/simple'
+THEME = './themes/thecodeship'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
