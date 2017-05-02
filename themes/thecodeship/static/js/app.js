ae4bd66fe11447677f1573ec4ec6bcba4a5e0a20
Ayman <ayman.farhat1@gmail.com>
2017-05-02 18:37:19 +0300

    Convert links inside articles to target=_blank

--- themes/thecodeship/static/js/app.js
+++ themes/thecodeship/static/js/app.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+// Convert all content links into target=_blank
+(function () {
+    var links = document.querySelectorAll('.entry-content a');
+
+    console.log(links);
+
+    for (var i = 0, length = links.length; i < length; i++) {
+        if (links[i].hostname != window.location.hostname) {
+            links[i].target = '_blank';
+        }
+    }
+})();
