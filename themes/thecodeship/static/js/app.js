03c92668d247c0d0ba2eb375e045d479797270ef
Ayman <ayman.farhat1@gmail.com>
2017-05-05 12:16:24 +0300

    Stick sidebar ad on scroll

--- themes/thecodeship/static/js/app.js
+++ themes/thecodeship/static/js/app.js
@@ -2,11 +2,25 @@
 (function () {
     var links = document.querySelectorAll('.entry-content a');
 
-    console.log(links);
-
     for (var i = 0, length = links.length; i < length; i++) {
         if (links[i].hostname != window.location.hostname) {
             links[i].target = '_blank';
         }
     }
+
+    var sideBar = document.getElementsByClassName('sidebar-ad-container'),
+        sideBarAdAnchor = sideBar[0];
+
+        var sidebarAdRect = sideBarAdAnchor.getBoundingClientRect(),
+            adContainerTop = sidebarAdRect.top + document.body.scrollTop;
+
+    window.addEventListener('scroll', function () {
+            bodyScroll = document.body.scrollTop;
+        
+        if (bodyScroll >= adContainerTop) {
+            sideBarAdAnchor.classList.add('stick');
+        } else {
+            sideBarAdAnchor.classList.remove('stick');
+        }
+    });
 })();
